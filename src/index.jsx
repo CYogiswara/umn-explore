d1762466adfc3d4505450e5e9c07c3386dce7c49
CYogiswara <112369336+CYogiswara@users.noreply.github.com>
2025-06-10 16:36:26 +0700

    Deployed website

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Page from './allMenu/Page';
 import Indo from './indonesia/Indo';
 import Japan from './japan/Japan';
