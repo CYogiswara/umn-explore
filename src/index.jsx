04c27c76bb786c3b38c6d52565762a8596d6d6aa
CYogiswara <112369336+CYogiswara@users.noreply.github.com>
2025-06-10 16:37:42 +0700

    Deployed website

--- src/index.jsx
+++ src/index.jsx
@@ -15,7 +15,7 @@ import Map from './allMenu/Map';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Page/>} />
         <Route path="/Indo" element={<Indo/>} />
@@ -25,7 +25,7 @@ root.render(
         <Route path="/Mesir" element={<Mesir/>} />
         <Route path="/Map" element={<Map/>} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
